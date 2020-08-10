c87ebbe5ea3e741ce23d0d6148f90c214974cc27
Nick Bykon <nikita.bikon@gmail.com>
2020-08-10 16:27:48 +0300

    add custom hook for server messages

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,12 +1,18 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import {useHttp} from '../hooks/http.hook'
+import {useMessage} from '../hooks/message.hook'
 
 export const AuthPage = () => {
-    const {loading, request} = useHttp()
+    const message = useMessage()
+    const {loading, request, error, clearError} = useHttp()
     const [form, setForm] = useState({
         email: '', password: ''
     })
 
+    useEffect(() => {
+        message(error)
+        clearError()
+    }, [error, message, clearError])
 
     const changeHandler = event => {
         setForm({ ...form, [event.target.name]: event.target.value })
@@ -15,7 +21,14 @@ export const AuthPage = () => {
     const registerHandler = async () => {
         try {
           const data = await request('/api/auth/register', 'POST', {...form})
-         console.log('Data', data)
+          message(data.message)
+        } catch (e) {}
+    }
+
+    const loginHandler = async () => {
+        try {
+          const data = await request('/api/auth/login', 'POST', {...form})
+          message(data.message)
         } catch (e) {}
     }
 
@@ -56,6 +69,7 @@ export const AuthPage = () => {
                     <button
                     className="btn yellow darken-4"
                     style={{marginRight: 10}}
+                    onClick={loginHandler}
                     disabled={loading}
                     >
                     Войти
